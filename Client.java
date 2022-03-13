cfc90f709919b9f511c269989e61e666eb9af011
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2022-03-13 15:50:53 +1100

    Create resource type. schd to top CPU resource(s).

--- Client.java
+++ Client.java
@@ -3,6 +3,7 @@ import java.net.UnknownHostException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Client {
     static String targetIP;
@@ -36,17 +37,33 @@ public class Client {
             sendMessage("GETSCapable" + job1.substring(13), outStream);  // Step 7, getting capable systems
             getInMsgString(inStream);  // Step 8, getting DATA/preparation message
             sendMessage("OK", outStream);  // Step 9, Sending OK for DATA we got preparation msg for in s8.
-            String resourceInfo = getInMsgString(inStream);  // Step 10, in this case receive resource information
+            String[] resources = parseCapability(getInMsgString(inStream));  // Step 10, in this case receive resource information
+            ArrayList<ServerResource> topCPU = getTopCPUList(resources);
             sendMessage("OK", outStream);  // Step 11, send OK and go back to step 10
             getInMsgString(inStream);  // Step 10 again, get "." i.e. no more info return to step 7.
-            sendMessage("SCHD0  "+resourceInfo.substring(0, 6), outStream);  // Step 7: Schedule first job (idx 0) to first resource in received resource info.
-            // sendMessage("SCHD0  super-silk 0", outStream);  // Hard coded just to see it go on a non joon server, it did.
+            sendMessage("SCHD0  "+topCPU.get(0).getName(), outStream);  // Step 7: Schedule first job (idx 0) to first resource in received resource info.
 
             checkInMessage("OK", inStream);  // Step 8: Check for OK scheduling confirmation from server.
             // Unsure of what step I am in below here, believe I should be at step 9 however
             // Am not receiving any data from the server.
             sendMessage("OK", outStream);  // Step 9: OK to get DS-server info/data (hopefully about jobs on server)
             getInMsgString(inStream);  // Step 10: hopefully info about jobs on server
+
+            // -------------------------------------------------------------------------- //
+
+
+            // sendMessage("REDY", outStream);
+            // String step6 = getInMsgString(inStream);
+            // while (step6 != "NONE") {
+            //     if (step6.substring(0, 4).equals("JOBN")) {
+
+                    
+            //     }
+                
+            //     // Need to ensure return to step 5 from step 8 
+            //     sendMessage("REDY", outStream);
+            //     step6 = getInMsgString(inStream);
+            // }
 
             // Gracefully close connection
             sendMessage("QUIT", outStream);
@@ -99,10 +116,6 @@ public class Client {
         return output;
     }
 
-    // private static String removeAllWhiteSpace(String input) {
-        
-    // }
-
     /**
      * Check for server response when response is known, such as in the initial greet sequence
      * where server should respond "OK".
@@ -132,4 +145,45 @@ public class Client {
             System.err.println("sendMessage: IOException - " + e.getMessage());
         }
     }
+
+    /**
+     * Parses servers GETSCapable response into an array of resource/capability Strings. 
+     * @param capableResponse
+     * @return String array of server capabilities/resources.
+     */
+    private static String[] parseCapability(String capableResponse) {
+        // String[] capable = capableResponse.split("\\R");
+        // System.out.println("parseCapabiltiy: Printing split string.");
+        // for (String s : capable) { System.out.println(s); }
+        // return capable;
+        return capableResponse.split("\\R");
+
+        /**
+         * Reference response to parse:
+         * joon 0 inactive -1 4 16000 64000 0 0
+         * joon 1 inactive -1 4 16000 64000 0 0
+         * super-silk 0 inactive -1 16 64000 512000 0 0
+         */
+    }
+
+    /**
+     * Provides list of server resources with highest CPU core count.
+     * @param inResources
+     * @return ArrList of resources with highest CPU core count.
+     */
+    private static ArrayList<ServerResource> getTopCPUList(String[] inResources) {
+        ArrayList<ServerResource> resources = new ArrayList<>();
+        int topCoreCount = 0;
+        for (String s : inResources) {
+            ServerResource sr = new ServerResource(s);
+            if (sr.getCoreCount() > topCoreCount) { topCoreCount = sr.getCoreCount(); }
+            resources.add(sr);
+        }
+        ArrayList<ServerResource> topCPU = new ArrayList<>();
+        for (ServerResource sr : resources) {
+            if (sr.getCoreCount() == topCoreCount) { topCPU.add(sr); }
+        }
+        return topCPU;
+    }
+
 } 
