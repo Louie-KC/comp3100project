bfb9dd8158ff8c81b704f1958c6e78b4a32301e1
LouieK-C <83757145+LouieK-C@users.noreply.github.com>
2022-03-30 21:22:16 +1100

    Remove unnecessary commented out fields for stage1

--- Job.java
+++ Job.java
@@ -1,18 +1,12 @@
 public class Job {
-    // private int submitTime;
     private int jobID;
-    // private int estRunTime;
     private int CPURequirement;
     private int RAMRequirement;
     private int diskSpaceRequirement;
 
-
     public Job(String jobString) {
         String[] data = jobString.split(" ");
-        // Ignore data[0] as it is "JOBN"
-        // submitTime = Integer.valueOf(data[1]);
         jobID = Integer.valueOf(data[2]);
-        // estRunTime = Integer.valueOf(data[3]);
         CPURequirement = Integer.valueOf(data[4]);
         RAMRequirement = Integer.valueOf(data[5]);
         diskSpaceRequirement = Integer.valueOf(data[6]);
@@ -26,7 +20,7 @@ public class Job {
      * Used for querying DS-SIM server about job capabilities.
      * @return String used after "GETS " to find server capability.
      */
-    public String getCapableString() {
+    public String getQueryString() {
         String result = CPURequirement + " " + RAMRequirement + " " + diskSpaceRequirement;
         return result;
     }
--- ServerResource.java
+++ ServerResource.java
@@ -1,36 +1,16 @@
 public class ServerResource {
     private String type;
     private int id;
-    // private String state;
-    // private int curStartTime;
     private int coreCount;
-    // private int memory;
-    // private int diskSpace;
-    // private int wJobs;
-    // private int rJobs;
 
     public ServerResource(String resourceString) {
         String[] data = resourceString.split(" ");
         type = data[0];
         id = Integer.valueOf(data[1]);
-        // state = data[2];
-        // curStartTime = Integer.valueOf(data[3]);
         coreCount = Integer.valueOf(data[4]);
-        // memory = Integer.valueOf(data[5]);
-        // diskSpace = Integer.valueOf(data[6]);
-        // wJobs = Integer.valueOf(data[7]);
-        // rJobs = Integer.valueOf(data[8]);
-
-        /**
-         * Format: "serverType serverID state curStartTime core memory disk #wJobs #rJobs"
-         * Reference:
-         * joon 0 inactive -1 4 16000 64000 0 0
-         * joon 1 inactive -1 4 16000 64000 0 0
-         * super-silk 0 inactive -1 16 64000 512000 0 0
-         */
     }
 
-    public String getServerType() {
+    public String getType() {
         return type;
     }
 
@@ -42,5 +22,3 @@ public class ServerResource {
         return type + " " + id;
     }
 }
-
-
